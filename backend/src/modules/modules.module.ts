ed46d12269a291b1f4e06ce1ff961e1e5f38c75a
Jhonatan Barata <henrique_lua16@hotmail.com>
2026-04-03 12:57:06 -0300

    feat: add master operational triage chips

--- backend/src/modules/modules.module.ts
+++ backend/src/modules/modules.module.ts
@@ -7,9 +7,10 @@ import { ModulesService } from './modules.service';
 import { ModuleAccessGuard } from './module-access.guard';
 import { MasterGuard } from '../auth/guards/master.guard';
 import { MasterContextModule } from '../master-context/master-context.module';
+import { CompaniesModule } from '../companies/companies.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, MasterContextModule, IntegrationsModule],
+  imports: [PrismaModule, UsersModule, MasterContextModule, IntegrationsModule, CompaniesModule],
   providers: [ModulesService, ModuleAccessGuard, MasterGuard],
   controllers: [ModulesController],
   exports: [ModulesService, ModuleAccessGuard],
