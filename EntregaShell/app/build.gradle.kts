823aaf0d8533ef2a8d51976c8ec47ae12fbc9bfc
JhonatanBarata <HENRIQUE_LUA16@HOTMAIL.COM>
2026-07-13 14:32:31 -0300

    chore: bump Android shell for paired login

--- EntregaShell/app/build.gradle.kts
+++ EntregaShell/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "br.com.hbxsystem"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     signingConfigs {
